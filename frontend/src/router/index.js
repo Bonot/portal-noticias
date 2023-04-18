ad7d539e3feab4e69110f957eab41aa48cb26d7c
bonot <paulabonot@gmail.com>
2023-04-18 04:47:11 -0300

    Implementa página de criação de autores

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AuthorsView from '../views/Authors/View.vue'
+import AuthorsCreate from '../views/Authors/Create.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/autores',
       name: 'authors',
       component: AuthorsView
+    },
+    {
+      path: '/autores/novo',
+      name: 'authorsCreate',
+      component: AuthorsCreate
     },
   ]
 })
