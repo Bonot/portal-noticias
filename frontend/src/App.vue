d21b26ce724790f934c5bcf002c28e6c61dc0cd5
bonot <paulabonot@gmail.com>
2023-04-18 17:04:15 -0300

    Corrige link navbar titulo

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,7 +8,7 @@ import { RouterLink, RouterView } from 'vue-router'
 
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-          <RouterLink class="navbar-brand" to="#">Portal de notícias</RouterLink>
+          <RouterLink class="navbar-brand" to="/">Portal de notícias</RouterLink>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
