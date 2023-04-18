b4cf6974e3d5e9abfd5c0b2edf57158e5d5a26e6
bonot <paulabonot@gmail.com>
2023-04-18 04:59:06 -0300

    Implementa página de edição de autores

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AuthorsView from '../views/Authors/View.vue'
 import AuthorsCreate from '../views/Authors/Create.vue'
+import AuthorsEdit from '../views/Authors/Edit.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/autores/novo',
       name: 'authorsCreate',
       component: AuthorsCreate
+    },
+    {
+      path: '/autores/:id/editar',
+      name: 'authorsEdit',
+      component: AuthorsEdit
     },
   ]
 })
