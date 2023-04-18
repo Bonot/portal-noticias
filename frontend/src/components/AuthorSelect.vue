1aeac9af61278965f44887330d12929bd0b60420
bonot <paulabonot@gmail.com>
2023-04-18 15:29:02 -0300

    Cria componente de seleção de autores

---
+++ frontend/src/components/AuthorSelect.vue
@@ -0,0 +1,36 @@
+<template>
+    <select class="form-select"
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
+    >
+        <option value="0" selected>Selecione um autor</option>
+        <option v-for="(author, index) in this.authors" :key="index"
+            :value="author.id">{{author.name}}</option>
+    </select>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    name: 'authorSelect',
+    props: ['modelValue'],
+    emits: ['update:modelValue'],
+    data(){
+        return {
+            authors: [],
+        }
+    },
+    mounted(){
+        this.getAuthors();
+    },
+    methods: {
+        getAuthors() {
+            axios.get(`http://localhost:90/api/authors`)
+                .then(response => {
+                    this.authors = response.data;
+                });
+        }
+    }
+}
+</script>
