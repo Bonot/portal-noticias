3ba64d413bc9b9c79e48797f3e616e55bea6e6bf
bonot <paulabonot@gmail.com>
2023-04-19 04:46:09 -0300

    Implementa auditoria de dados

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -6,11 +6,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class Article extends Model
+class Article extends Model implements Auditable
 {
     use HasFactory;
     use SoftDeletes;
+    use \OwenIt\Auditing\Auditable;
 
     protected $fillable = [
         'title',
