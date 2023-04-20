efc709bbc92c2d5cf4ea9540c6b438fce7e271f0
bonot <paulabonot@gmail.com>
2023-04-20 12:57:33 -0300

    Cria autenticação das rotas da API

---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Response;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => [
+                'required',
+                'email'
+            ],
+            'password' => [
+                'required',
+            ]
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'status' => Response::HTTP_UNPROCESSABLE_ENTITY,
+            'errors' => $validator->errors()
+        ]));
+    }
+
+    public function messages()
+    {
+        return [
+            'email.required' => 'Informe um email.',
+            'email.email' => 'Informe um email válido.',
+            'password.required' => 'Informe uma senha.',
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\ArticleController;
 use App\Http\Controllers\Api\AuthorController;
+use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -21,7 +22,21 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('authors/resource', [AuthorController::class, 'getResource']);
-Route::apiResource('/authors', AuthorController::class);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('authors/resource', [AuthorController::class, 'getResource']);
+    Route::apiResource('/authors', AuthorController::class);
+    Route::apiResource('/articles', ArticleController::class);
+});
 
-Route::apiResource('/articles', ArticleController::class);
+Route::post('/login', function (LoginRequest $request) {
+    $credentials = $request->only(['email', 'password']);
+
+    if (Auth::attempt($credentials) === false) {
+        return response()->json('Unauthorized', 401);
+    }
+
+    $user = Auth::user();
+    $token = $user->createToken(name: 'token');
+
+    return response()->json($token->plainTextToken);
+});
